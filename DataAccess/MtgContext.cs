75d4664a7b765d7db91a071c75a655bddf5d72e6
wpelczar <wpelczar1@gmail.com>
2017-07-06 21:46:41 +0200

    Installed ef core

---
+++ DataAccess/MtgContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTG.Scores2.Models;
+
+namespace MTG.Scores2.DataAccess
+{
+  public class MtgContext : DbContext
+  {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+      base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<Match> Matches { get; set; }
+
+    public DbSet<Player> Players { get; set; }
+
+    
+  }
+}
