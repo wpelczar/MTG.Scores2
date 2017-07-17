acf5ccdfbabbb747412546b8718981363b77047f
wpelczar <wpelczar1@gmail.com>
2017-07-17 21:10:10 +0200

    Add MatchService to server providers

--- ClientApp/app/app.module.server.ts
+++ ClientApp/app/app.module.server.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { sharedConfig } from './app.module.shared';
+import { MatchService } from './components/scores/match.service';
 
 @NgModule({
     bootstrap: sharedConfig.bootstrap,
     declarations: sharedConfig.declarations,
+    providers:[
+        MatchService,
+    ],
     imports: [
         ServerModule,
         ...sharedConfig.imports
