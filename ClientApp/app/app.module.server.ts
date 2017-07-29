b3f85d815882e1bc39b6520585379b1110bf13e8
wpelczar <wpelczar1@gmail.com>
2017-07-29 22:21:34 +0200

    missing RankingService provider

--- ClientApp/app/app.module.server.ts
+++ ClientApp/app/app.module.server.ts
@@ -4,13 +4,15 @@ import { sharedConfig } from './app.module.shared';
 import { MatchService } from './components/scores/match.service';
 import { PlayerService } from './components/scores/player.service';
 import { FormsModule } from '@angular/forms';
+import { RankingService } from './components/ranking/ranking.service';
 
 @NgModule({
     bootstrap: sharedConfig.bootstrap,
     declarations: sharedConfig.declarations,
     providers:[
         MatchService,
-        PlayerService
+        PlayerService,
+        RankingService
     ],
     imports: [
         FormsModule,
