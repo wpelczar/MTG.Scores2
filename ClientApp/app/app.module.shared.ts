fb951f0ca4b3c93fb3c4e3be013046da19b37d8e
Unknown <wpelczar1@gmail.com>
2018-05-19 15:12:09 +0200

    Material nav menu

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component'
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { ScoresComponent } from './components/scores/scores.component';
 import { MatchFilterByPlayerPipe } from './components/scores/match-filter-by-player.pipe';
 import { RankingComponent } from './components/ranking/ranking.component';
@@ -14,7 +13,6 @@ export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
-        NavMenuComponent,
         ScoresComponent,
         RankingComponent,
         MatchFilterByPlayerPipe,
