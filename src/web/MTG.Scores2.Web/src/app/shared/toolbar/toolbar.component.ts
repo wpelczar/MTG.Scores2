9c8f85824f2f8bf3232b2eeca31c122d70dd0bbb
Wojciech Pelczar <wpelczar1@gmail.com>
2019-10-06 15:03:24 +0200

    login/logout button

--- src/web/MTG.Scores2.Web/src/app/shared/toolbar/toolbar.component.ts
+++ src/web/MTG.Scores2.Web/src/app/shared/toolbar/toolbar.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, OnDestroy } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnInit, OnDestroy {
+  isAuthenticated: boolean;
+  authSubscription: Subscription;
+
   @Input() title: string;
 
   @Output() toggleSidenav = new EventEmitter<void>();
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authNavStatus$.subscribe(status => this.isAuthenticated = status);
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
+
+  signout() {
+    this.authService.signout();
   }
 }
