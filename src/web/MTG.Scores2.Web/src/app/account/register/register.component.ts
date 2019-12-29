bbba99338e38ebfc897da737efc7f046db07b3ab
Wojciech Pelczar <wpelczar1@gmail.com>
2019-12-29 23:30:53 +0100

    Fix validation on register form

--- src/web/MTG.Scores2.Web/src/app/account/register/register.component.ts
+++ src/web/MTG.Scores2.Web/src/app/account/register/register.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { finalize } from 'rxjs/operators'
+import { finalize } from 'rxjs/operators';
 import { UserRegistration } from 'src/app/shared/models/user-registration';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
+function passwordValidator(c: AbstractControl): { [key: string]: boolean } | null {
+  const pass = c.value as string;
+  const passRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,})');
+
+  if (!pass || !passRegex.test(pass)) {
+      return {'passwordFormat': true};
+  }
+
+  return null;
+}
 
 @Component({
   selector: 'app-register',
@@ -14,20 +25,33 @@ export class RegisterComponent implements OnInit {
 
   success: boolean;
   error: boolean;
-  userRegistration: UserRegistration = { name: '', email: '', password: '' };
-  submitted = false;
 
-  constructor(private authService: AuthService, private spinner: NgxSpinnerService) {
+  submitted = false;
+  registerForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private spinner: NgxSpinnerService,
+    private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [passwordValidator]]
+    });
   }
 
   onSubmit() {
+    if (!this.registerForm.valid) {
+      return;
+    }
+
     this.error = false;
     this.spinner.show();
 
-    this.authService.register(this.userRegistration)
+    this.authService.register(<UserRegistration>this.registerForm.value)
       .pipe(finalize(() => {
         this.spinner.hide();
       }))
